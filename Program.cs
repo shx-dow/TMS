f08ee92f26a4d41b4d491d7151a3f3919b6db01d
shx-dow <83426772+shx-dow@users.noreply.github.com>
2025-06-09 19:06:57 +0530

    basic setup

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using TMS.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=tasks.db"));
 
 var app = builder.Build();
 
---
+++ models/TaskItem.cs
@@ -0,0 +1,29 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Models
+{
+    public enum PriorityLevel
+    {
+        Low,
+        Medium,
+        High
+    }
+
+    public class TaskItem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DueDate { get; set; }
+
+        public bool IsComplete { get; set; }
+
+        public PriorityLevel Priority { get; set; }
+    }
+}
