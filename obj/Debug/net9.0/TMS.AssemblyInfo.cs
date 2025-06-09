683b02fe46eaf15bb060d3051a8d218cf4a23db2
shx-dow <83426772+shx-dow@users.noreply.github.com>
2025-06-09 19:27:16 +0530

    Add bootsrap and apply basic styling to task pages

--- obj/Debug/net9.0/TMS.AssemblyInfo.cs
+++ obj/Debug/net9.0/TMS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9485844d5ae8f54631733719227d53e84c471fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e29bd786159e855aba993b5e406897a28319055a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
